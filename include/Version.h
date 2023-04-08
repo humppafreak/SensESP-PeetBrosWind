62a0beb827589b0ce9c248726aee22121a104998
Tobias C. Rosenstock <dev@air-discordia.org>
2023-04-09 01:52:10 +0200

    added SensESP dependencies

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.1.0"
+        #define VERSION "0.1.2"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2023-04-09 01:11:26.599243"
+        #define BUILD_TIMESTAMP "2023-04-09 01:31:09.604742"
     #endif
     
