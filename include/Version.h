f865b69904ea8c67a32a534916ac6a3fc7276eb5
Tobias C. Rosenstock <dev@air-discordia.org>
2023-04-14 21:27:44 +0200

    bump version to 0.3.2

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.3.1"
+        #define VERSION "0.3.2"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2023-04-14 18:24:37.824026"
+        #define BUILD_TIMESTAMP "2023-04-14 21:21:44.597826"
     #endif
     
